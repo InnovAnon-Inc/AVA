55feb6a6ad54e0ad156cb29f57da47e6f4827e26
Default User <laurence.a.maddox@gmail.com>
2017-09-18 04:52:15 -0500

    bugfix

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <solar-common.h>
+
 #include <ava.h>
 
 __attribute__ ((leaf, nonnull (1), nothrow, warn_unused_result))
@@ -9,11 +11,24 @@ static int cb (void *arg) {
 	return 0;
 }
 
-int main (void) {
-	coord_t coord;
-	coord.latitude = ?;
-	coord.longitude = ?;
-	coord.altitude = ?;
+__attribute__ ((nonnull (2), nothrow))
+int main (int argc, char *argv[]) {
+	struct coord_t coord;
+	double zenith;
+	int localoffset;
+	error_check (argc != 6) {
+		printf ("Usage: %s <latitude> <longitude> <altitude> "
+		        "<zenith> <local offset>\n",
+		      argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	error_check (parseDouble (&(coord.latitude),  argv[1]) != 0) return EXIT_FAILURE;
+	error_check (parseDouble (&(coord.longitude), argv[2]) != 0) return EXIT_FAILURE;
+	error_check (parseDouble (&(coord.altitude),  argv[3]) != 0) return EXIT_FAILURE;
+	error_check (parseDouble (&zenith,      argv[4]) != 0) return EXIT_FAILURE;
+	error_check (parseInt    (&localoffset, argv[5]) != 0) return EXIT_FAILURE;
+
 	error_check (set_ava (&coord, cb, (void *) "Hello, World!") != 0)
 		return EXIT_FAILURE;
 	return EXIT_SUCCESS;

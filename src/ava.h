29a4babaf097b65011840f488365f7b654282a86
Default User <laurence.a.maddox@gmail.com>
2017-09-19 18:23:55 -0500

    using a different astronomy backend

--- src/ava.h
+++ src/ava.h
@@ -5,11 +5,17 @@
 extern "C" {
 #endif
 
+#include <libnova/ln_types.h>
+
 #include <glitter.h>
 /*#include <solar.h>
 
-int set_ava (coord_t *restrict coord, stdcb_t cb, void *arg)
-__attribute__ ((leaf, nonnull (1, 2), warn_unused_result)) ;*/
+int set_ava (coord_t *restrict coord, stdcb_t cb, void *arg)*/
+int set_ava (
+	struct ln_lnlat_posn *restrict observer,
+	stdcb_t cb,
+	void *arg)
+__attribute__ ((leaf, nonnull (1, 2), warn_unused_result)) ;
 
 #ifdef __cplusplus
 }

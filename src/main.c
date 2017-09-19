6a6b925d8aaa4700ff27a16c4c59ee21c963e672
Default User <laurence.a.maddox@gmail.com>
2017-09-19 18:28:57 -0500

    using a different astronomy backend

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <solar-common.h>
-
 #include <ava.h>
 
 __attribute__ ((nonnull (1), nothrow, warn_unused_result))
@@ -13,21 +11,21 @@ static int cb (void *arg) {
 
 __attribute__ ((nonnull (2), nothrow))
 int main (int argc, char *argv[]) {
-	coord_t coord;
+	struct ln_lnlat_posn coord;
 	double zenith;
 	int localoffset;
-	error_check (argc != 5) {
-		printf ("Usage: %s <latitude> <longitude> <altitude>\n", argv[0]);
+	error_check (argc != 4) {
+		printf ("Usage: %s <latitude> <longitude> <message>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	error_check (parseDouble (&(coord.latitude),  argv[1]) != 0) return EXIT_FAILURE;
-	error_check (parseDouble (&(coord.longitude), argv[2]) != 0) return EXIT_FAILURE;
-	error_check (parseDouble (&(coord.altitude),  argv[3]) != 0) return EXIT_FAILURE;
+	error_check (parseDouble (&(coord.lat),  argv[1]) != 0) return EXIT_FAILURE;
+	error_check (parseDouble (&(coord.lng), argv[2]) != 0) return EXIT_FAILURE;
+	/*error_check (parseDouble (&(coord.altitude),  argv[3]) != 0) return EXIT_FAILURE;*/
 	/*error_check (parseDouble (&zenith,      argv[4]) != 0) return EXIT_FAILURE;*/
 	/*error_check (parseInt    (&localoffset, argv[5]) != 0) return EXIT_FAILURE;*/
 
-	error_check (set_ava (&coord, cb, argv[4]) != 0)
+	error_check (set_ava (&coord, cb, argv[3]) != 0)
 		return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }

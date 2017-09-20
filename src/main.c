c1a56384c3a730774fa145f2f39285a10898cbed
Default User <laurence.a.maddox@gmail.com>
2017-09-19 21:07:42 -0500

    test

--- src/main.c
+++ src/main.c
@@ -1,4 +1,74 @@
 #include <stdio.h>
+#include <libnova/solar.h>
+#include <libnova/julian_day.h>
+#include <libnova/rise_set.h>
+#include <libnova/transform.h>
+
+static void print_date (char const title[], struct ln_zonedate* date)
+{
+	printf ("\n%s\n",title);
+	printf (" Year    : %d\n", date->years);
+	printf (" Month   : %d\n", date->months);
+	printf (" Day     : %d\n", date->days);
+	printf (" Hours   : %d\n", date->hours);
+	printf (" Minutes : %d\n", date->minutes);
+	printf (" Seconds : %f\n", date->seconds);
+}
+
+int main (int argc, char * argv[])
+{
+	struct ln_equ_posn equ;
+	struct ln_rst_time rst;
+	struct ln_zonedate rise, set, transit;
+	struct ln_lnlat_posn observer;
+	struct ln_helio_posn pos;
+	double JD;
+
+	/* observers location (Edinburgh), used to calc rst */
+	observer.lat = 55.92; /* 55.92 N */
+	observer.lng = -3.18; /* 3.18 W */
+
+	/* get Julian day from local time */
+	JD = ln_get_julian_from_sys();
+	printf ("JD %f\n", JD);
+
+	/* geometric coordinates */
+	ln_get_solar_geom_coords (JD, &pos);
+	printf("Solar Coords longitude (deg) %f\n", pos.L);
+	printf("             latitude (deg) %f\n", pos.B);
+	printf("             radius vector (AU) %f\n", pos.R);
+
+	/* ra, dec */
+	ln_get_solar_equ_coords (JD, &equ);
+	printf("Solar Position RA %f\n", equ.ra);
+	printf("               DEC %f\n", equ.dec);
+
+	/* rise, set and transit */
+	if (ln_get_solar_rst (JD, &observer, &rst) == 1)
+		printf ("Sun is circumpolar\n");
+	else {
+		ln_get_local_date (rst.rise, &rise);
+		ln_get_local_date (rst.transit, &transit);
+		ln_get_local_date (rst.set, &set);
+		print_date ("Rise", &rise);
+		print_date ("Transit", &transit);
+		print_date ("Set", &set);
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+#ifndef NWTF
+
+#include <stdio.h>
 #include <stdlib.h>
 
 #include <ezparse.h>
@@ -31,3 +101,4 @@ int main (int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }
+#endif
